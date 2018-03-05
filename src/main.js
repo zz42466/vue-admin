5b1f2d1629554eb2a8440b9e722710a275d55cc7
zz42466 <zz42466@163.com>
2018-03-05 17:18:24 +0800

    change file name

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/css/font-awesome.min.css';
-import App from './App';
+import App from './app';
 import router from './router';
 
 Vue.use(ElementUI);
