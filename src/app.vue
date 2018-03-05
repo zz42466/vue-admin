9fd55b6bb09fecbd62ad45649571937cd11a43e2
zz42466 <zz42466@163.com>
2018-03-05 17:20:27 +0800

    change file name

---
+++ src/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <MainPage />
+  </div>
+</template>
+
+<script>
+  import MainPage from './components/main-page';
+
+  export default {
+    name: 'App',
+    components: { MainPage }
+  };
+</script>
